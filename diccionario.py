d9c1ce78fa0a353e1c0f16ba5b6817b3a954bdf1
MerinNoxus <merinfap11@gmail.com>
2023-02-10 12:22:48 -0600

    ya casi esta el lexico

---
+++ diccionario.py
@@ -0,0 +1,55 @@
+#ARTICULOS
+definite_singular_articles_lower={'el':'masculino singular','la':'femenino singular','lo':'neutro singular','los':'masculino plural','las':'femenino plural'}
+definite_singular_articles_lower__KEY=definite_singular_articles_lower.keys()
+
+indefinite_singular_articles_lower={'un':'masculino singular','una':'femenino singular','unos':'masculino plural','unas':'femenino plural','de':'neutro singular'}
+indefinite_singular_articles_lower__KEY=indefinite_singular_articles_lower.keys()
+
+definite_singular_articles_upper={'El':'masculino singular','La':'femenino singular','Lo':'neutro singular','Los':'masculino plural','Las':'femenino plural'}
+definite_singular_articles_upper__KEY=definite_singular_articles_upper.keys()
+
+indefinite_singular_articles_upper={'Un':'masculino singular','Una':'femenino singular','Unos':'masculino plural','Unas':'femenino plural'}
+indefinite_singular_articles_upper__KEY=indefinite_singular_articles_upper.keys()
+
+#SUJETOS O SUSTANTIVOS
+subjects={
+'niño':'masculino, raza humana XD, singular',
+'mariposas':'femenino, lepidopteros, plural',
+'libélula':'femenino, anisópteros, singular',
+'perro':'masculino, canis lupus, singular',
+'pájaro':'masculino, ave vertebrada, singular',
+'abeja':'femenino, antófilos, singular',
+'manzana':'femenino, malus domestica, singular',
+'computadora':'femenino, máquina electrónica digital programable, singular',
+'leones':'masculino, felinos, plural',
+'golondrinas':'femenino, hirundo rustica, plural'
+}
+subjects__KEY=subjects.keys()
+
+#VERBS
+verbs = {
+'vuelan':'elevarse, estar en el aire, tercera persona del plural',
+'ladra':'sonido fuerte, atencion, infinitivo',
+'come': 'alimentarse, infinitivo',
+'trabaja':'actividad fisica y/o mental, infinitivo',
+'salta':'despegarse del suelo, infinitivo',
+'cantan':'sonido melodico, tercera persona del plural',
+'juegan':'actividad divertida, tercera persona del plural',
+'llora':'accion no feliz, infinitivo',
+'murió' :'vida invertida, tercera persona del singular',
+'caga':'defecar vulgarmente, inifinitivo',
+'comen':'alimentarse, tercera persona del plural',
+'vuela':'elevarse, estar en el aire, primera persona del singular'
+}
+verbs__KEY=verbs.keys()
+
+#PREDICATE
+words ={
+''
+}
+
+#symbols
+symbols = {
+    ' ': 'espacio',
+    '.':'punto'
+}
